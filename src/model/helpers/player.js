39bcea80bde93f9cff6db646be452c8bbeca3a9c
Welliton Gervickas <welliton@send4.com.br>
2018-09-28 12:00:28 -0300

    Fix player percentage

--- src/model/helpers/player.js
+++ src/model/helpers/player.js
@@ -7,9 +7,5 @@
 export function getPlayerPercentage(current, max = 30) {
 
   let currentTime = Math.round(current.toFixed(1));
-  let percentage = Math.round(currentTime * 100 / max);
-
-  console.log(percentage);
-
-  return percentage;
+  return Math.round(currentTime * 100 / max);
 };
