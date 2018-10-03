65bc258b76333df58fb801e8fab1a4175454b90a
Welliton Gervickas <welliton@send4.com.br>
2018-10-03 16:07:28 -0300

    Fix route history

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-
+import { Router } from 'react-router-dom';
 import Header from './components/header';
 import Content from './components/content';
 import Footer from './components/footer';
 import Loading from '../components/loading';
-
+import { history } from '../store';
 
 const App = () => (
-  <div className="app">
-    <Header/>
-    <Content />
-    <Footer />
-    <Loading />
-  </div>
+  <Router key="dashboard" history={ history }>
+    <div className="app">
+      <Header/>
+      <Content />
+      <Footer />
+      <Loading />
+    </div>
+  </Router>
 );
 
 export default App;
